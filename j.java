bb9ce0836f5644f3655765be1d8241d97506c1f3
Proxigo35 <andrei.t35@outlook.com>
2025-04-28 12:09:15 +0100

    Fixed counting sort algorithm.

--- j.java
+++ j.java
@@ -26,6 +26,7 @@ public class j {
 		int temp;
 		int iterations = 0;
 		boolean unsorted = true;
+		
 		while (unsorted) {
 			unsorted = false;
 			for (int i = 0; i < array.length - 1 - iterations; i++) {
@@ -70,6 +71,7 @@ public class j {
 		long startTime = System.nanoTime();
 		int temp;
 		int j = 0;
+		
 		for (int i = 1; i < array.length; i++) {
 			j = i;   
 			while (j >= 1) {
@@ -88,12 +90,23 @@ public class j {
 	
 	public static long quickSort(int[] array) {
 		long startTime = System.nanoTime();
+		
+		
+		
 		return System.nanoTime() - startTime;
 	}
 	
 	public static long countingSort(int[] array) {
 		long startTime = System.nanoTime();
-		int[] counts =  new int[array.length + 1];
+		int max = 0;
+		
+		for (int i : array) {
+			if (i > max) {
+				max = i;
+			}
+		}
+		
+		int[] counts =  new int[max + 1];
 		int index = 0;
 
 		for (int i : array) {
@@ -101,18 +114,19 @@ public class j {
 		}
 		
 		for (int i = 0; i < counts.length; i++) {
-			for (int j = index; j < i; j++) {
-				array[j] = i;
+			for (int j = 0; j < counts[i]; j++) {
+				array[index] = i;
 				index++;
 			}
+			
 		}
-		
 		return System.nanoTime() - startTime;
 	}
 	
 	public static void main(String[] args) {
+		
 		int[] sampleSize = {100, 250, 500, 750, 1000, 1250, 2500, 3750, 5000, 6250, 7500, 8750, 10000};
-		int numRuns = 1;
+		int numRuns = 10;
 		long startTime;
 		long totalTime;
 		int[] array;
@@ -135,7 +149,6 @@ public class j {
 			long countingSortTime = 0;
 			
 			for (int i = 0; i < numRuns; i++) {
-				
 				array = randomArray(n);
 				bubbleSortTime += bubbleSort(Arrays.copyOf(array, array.length));
 				selectionSortTime += selectionSort(Arrays.copyOf(array, array.length));
